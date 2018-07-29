d9fe5e0c1650d7845501713fc8e5ab245b0ce265
William Taylor <musicmrman99@gmail.com>
2018-07-29 17:29:11 +0100

    Remove unneeded ReactDOM import

--- src/panel.jsx
+++ src/panel.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default class Panel extends React.Component {
     render() { 
         return (
-            <div className="panel span span-2"></div>
+            <div className="panel span span-2">
+
+            </div>
         );
     }
 }
