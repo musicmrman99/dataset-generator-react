b1f9f2edbaa41dd283b31044710866423b1d7350
William Taylor <musicmrman99@gmail.com>
2018-08-03 18:25:00 +0100

    Remove additional space

--- src/workspace.jsx
+++ src/workspace.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default class Workspace extends React.Component {
-    render() { 
+    render() {
         return (
             <div className="workspace span span-10"></div>
         );
